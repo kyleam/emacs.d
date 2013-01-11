f5b226cae165532ed34ffefd7d003d10d984f004
Kyle Meyer <meyerkya@gmail.com>
2013-01-11 18:13:03 -0500

    defun to load config files

--- init.el
+++ init.el
@@ -1,19 +1,36 @@
 ;; init.el
 
 (message ":: Emacs init")
+
 (server-start)
 (add-to-list 'load-path "~/.emacs.d/conf")
-(load "km-pkg.el")
-(load "km-ui.el")
-(load "km-general.el")
-(load "km-editor.el")
-(load "km-func.el")
-(load "km-org.el")
-(load "km-evil.el")
-(load "km-ibuffer.el")
-(load "km-ido.el")
-(load "km-mail.el")
-(load "km-python.el")
-(load "km-haskell.el")
-(load "km-tex.el")
+
+(defvar km/config-files '(
+                          "km-pkg.el"
+                          "km-ui.el"
+                          "km-general.el"
+                          "km-editor.el"
+                          "km-func.el"
+                          "km-org.el"
+                          "km-evil.el"
+                          "km-ibuffer.el"
+                          "km-ido.el"
+                          "km-mail.el"
+                          "km-python.el"
+                          "km-haskell.el"
+                          "km-tex.el"
+                          )
+  "configuration files")
+
+(defun km/load-config-files ()
+  (dolist (cfile km/config-files)
+        (load cfile)))
+
+(defun km/load-config-files2 ()
+  (dolist (cfile km/config-files)
+        (message "loading %s" cfile)
+        (load cfile)))
+
+(km/load-config-files)
+
 (message ":: Init complete")
