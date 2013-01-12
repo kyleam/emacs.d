96ee4bd0bd7b5cc8a6d9ca918ede26e82a9b20a7
Kyle Meyer <meyerkya@gmail.com>
2013-01-12 12:44:44 -0500

    glob init files files for loading

--- init.el
+++ init.el
@@ -1,36 +1,11 @@
 ;; init.el
 
-(message ":: Emacs init")
+(message ":: emacs init")
 
 (server-start)
-(add-to-list 'load-path "~/.emacs.d/conf")
 
-(defvar km/config-files '(
-                          "km-pkg.el"
-                          "km-ui.el"
-                          "km-general.el"
-                          "km-editor.el"
-                          "km-func.el"
-                          "km-org.el"
-                          "km-evil.el"
-                          "km-ibuffer.el"
-                          "km-ido.el"
-                          "km-mail.el"
-                          "km-python.el"
-                          "km-haskell.el"
-                          "km-tex.el"
-                          )
-  "configuration files")
+(mapcar
+ (lambda (f) (load-file f))
+ (file-expand-wildcards "~/.emacs.d/init/*.el"))
 
-(defun km/load-config-files ()
-  (dolist (cfile km/config-files)
-        (load cfile)))
-
-(defun km/load-config-files2 ()
-  (dolist (cfile km/config-files)
-        (message "loading %s" cfile)
-        (load cfile)))
-
-(km/load-config-files)
-
-(message ":: Init complete")
+(message ":: init complete")
