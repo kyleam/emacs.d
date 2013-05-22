15dbf77596d5e416d8aa358ab02af796e7915234
Kyle Meyer <meyerkya@gmail.com>
2013-05-21 20:08:12 -0400

    bind kill-buffer-and-window
    C-x K was bound to ido-kill-buffer, which is already bound to C-x k
    
    this function is also bound to  C-x 4 0

--- init/km-keybindings.el
+++ init/km-keybindings.el
@@ -1,3 +1,5 @@
 ;; keybindings that don't go with other topics
 (global-set-key (kbd "C-x \\") 'align-regexp)
 (global-set-key (kbd "C-c i") 'indent-relative)
+
+(global-set-key (kbd "C-x K") 'kill-buffer-and-window)
