4aafad42e2ef3bcdcc41310fa42eea5d21b51171
Kyle Meyer <meyerkya@gmail.com>
2013-06-11 21:25:43 -0400

    org contacts: file to inbox by default

--- init/km-orgcontacts.el
+++ init/km-orgcontacts.el
@@ -3,5 +3,5 @@
 (setq org-contacts-files '("~/notes/contacts.org"))
 
 (add-to-list 'org-capture-templates
-             '("a" "email address" entry (file "~/notes/contacts.org")
+             '("a" "email address" entry (file+headline "~/notes/contacts.org" "Inbox")
                "** %(org-contacts-template-name)\n   :PROPERTIES:\n   :EMAIL: %(org-contacts-template-email)\n   :END:"))
