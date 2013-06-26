227a5f304495520b7771682e387abc83d06bd265
Kyle Meyer <meyerkya@gmail.com>
2013-06-26 16:02:43 -0400

    rebind backward-kill-word

--- init/km-keybindings.el
+++ init/km-keybindings.el
@@ -8,3 +8,5 @@
 ;; overrides `suspend-emacs' (which is also bound to C-x C-z)
 (global-set-key (kbd "C-z") 'zap-to-char)
 (global-unset-key (kbd "M-z"))
+
+(global-set-key (kbd "C-'") 'backward-kill-word)
