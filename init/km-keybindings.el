5bd185dbd79544c431a1635af34f1e13b0d12601
Kyle Meyer <meyerkya@gmail.com>
2013-06-26 15:53:57 -0400

    rebind zap-to-char

--- init/km-keybindings.el
+++ init/km-keybindings.el
@@ -1,5 +1,10 @@
 ;; keybindings that don't go with other topics
+
 (global-set-key (kbd "C-x \\") 'align-regexp)
 (global-set-key (kbd "C-c i") 'indent-relative)
 
 (global-set-key (kbd "C-x K") 'kill-buffer-and-window)
+
+;; overrides `suspend-emacs' (which is also bound to C-x C-z)
+(global-set-key (kbd "C-z") 'zap-to-char)
+(global-unset-key (kbd "M-z"))
