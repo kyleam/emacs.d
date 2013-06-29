54577f4f7ccd88348a31fe0c8893bf203bcf4926
Kyle Meyer <meyerkya@gmail.com>
2013-06-29 01:35:43 -0400

    move python shebang/common imports to yasnippet

--- init/km-python.el
+++ init/km-python.el
@@ -1,25 +1,3 @@
-(defun km/python-shebang ()
-  (interactive)
-  (km/shebang "python"))
-
-(defconst km/python-analysis-imports
-  "import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib import ticker
-
-from colors import brew, fgry, bgry")
-
-(defun km/python-insert-analysis-imports ()
-  "Common imports for analysis scripts"
-  (interactive)
-  (insert km/python-analysis-imports))
-
-(defun km/python-hook ()
-  (local-set-key (kbd "C-c p s") 'km/python-shebang)
-  (local-set-key (kbd "C-c p a") 'km/python-insert-analysis-imports))
-(add-hook 'python-mode-hook 'km/python-hook)
-
 ;; http://www.emacswiki.org/emacs/PythonProgrammingInEmacs#toc5
 (setq
  python-shell-interpreter "ipython"
