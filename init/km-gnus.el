b33e5f37e8a01ef0d65267ea7214d2fbac3dda61
Kyle Meyer <meyerkya@gmail.com>
2013-07-11 15:07:21 -0400

    add orgstruct to message-mode hook

--- init/km-gnus.el
+++ init/km-gnus.el
@@ -57,7 +57,8 @@
 
 (add-hook 'message-mode-hook
           '(lambda ()
-             (flyspell-mode 1)))
+             (flyspell-mode 1)
+             (orgstruct-mode 1)))
 
 (setq gnus-confirm-mail-reply-to-news t)
 
