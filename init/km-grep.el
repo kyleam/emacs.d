3cf4269136a316c10aa018d333d889b7e837fac6
Kyle Meyer <meyerkya@gmail.com>
2013-07-14 22:06:45 -0400

    hide grep header

---
+++ init/km-grep.el
@@ -0,0 +1,12 @@
+;; http://stackoverflow.com/questions/16122801/
+;; remove-header-information-from-rgrep-grep-output-in-emacs
+(defun hide-grep-header ()
+  (save-excursion
+    (with-current-buffer grep-last-buffer
+      (goto-line 5)
+      (narrow-to-region (point) (point-max)))))
+
+(defadvice grep (after hide-grep-header activate) (hide-grep-header))
+(defadvice rgrep (after hide-grep-header activate) (hide-grep-header))
+(defadvice lgrep (after hide-grep-header activate) (hide-grep-header))
+(defadvice grep-find (after hide-grep-header activate) (hide-grep-header))
