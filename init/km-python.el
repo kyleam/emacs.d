025f5f02180a80f7f36494f8a670755425f8eb32
Kyle Meyer <meyerkya@gmail.com>
2013-07-19 18:51:37 -0400

    add py.test compiling keybindings

--- init/km-python.el
+++ init/km-python.el
@@ -11,3 +11,13 @@
    "';'.join(module_completion('''%s'''))\n"
    python-shell-completion-string-code
    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
+
+(defun km/python-hook ()
+  (local-set-key (kbd "C-c m t") '(lambda ()
+                                    (interactive)
+                                    (compile "py.test")))
+  (local-set-key (kbd "C-c m T") '(lambda ()
+                                    (interactive)
+                                    (compile "py.test2"))))
+
+(add-hook 'python-mode-hook 'km/python-hook)
