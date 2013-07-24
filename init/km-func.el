ec6d4784e7c4e4ade14cefb1da561c32300949fa
Kyle Meyer <meyerkya@gmail.com>
2013-07-24 12:31:05 -0400

    Add swap-windows from prelude
    I tend handle more complicated window configurations with a tiling
    window manager, so I don't think I'd have much use for anything more
    complex (like transpose-frame.el).

--- init/km-func.el
+++ init/km-func.el
@@ -130,3 +130,23 @@ KILLARG."
 (ad-activate 'recompile)
 
 (global-set-key (kbd "C-c g") 'recompile)
+
+;; from prelude
+(defun km/swap-windows ()
+  "If you have 2 windows, it swaps them."
+  (interactive)
+  (if (/= (count-windows) 2)
+      (message "You need exactly 2 windows to do this.")
+    (let* ((w1 (car (window-list)))
+           (w2 (cadr (window-list)))
+           (b1 (window-buffer w1))
+           (b2 (window-buffer w2))
+           (s1 (window-start w1))
+           (s2 (window-start w2)))
+      (set-window-buffer w1 b2)
+      (set-window-buffer w2 b1)
+      (set-window-start w1 s2)
+      (set-window-start w2 s1)))
+  (other-window 1))
+
+(global-set-key (kbd "C-c s") 'km/swap-windows)
