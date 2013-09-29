b3e66da7e2f798db0d3bb5ee551b9cf42fd6ed45
Kyle Meyer <kyle@kyleam.com>
2013-09-28 23:34:28 -0400

    Clean up km-tex.el

--- init/km-tex.el
+++ init/km-tex.el
@@ -8,13 +8,7 @@
          "/usr/texbin" ":"
          (getenv "PATH")))
 
-;; commented out the rest of it because something was causing it to
-;; prompt for master file in org mode
 (defun km/org-mode-reftex-setup ()
-  (load-library "reftex")
-  ;; (and (buffer-file-name)
-  ;;      (file-exists-p (buffer-file-name))
-  ;;      (reftex-parse-all))
   (define-key org-mode-map (kbd "C-c [") 'reftex-citation))
 
 (add-hook 'org-mode-hook 'km/org-mode-reftex-setup)
