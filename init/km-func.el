f21775df2bd7c405d755471f27cdb1e9aeeb8f2d
Kyle Meyer <kyle@kyleam.com>
2013-10-27 21:25:46 -0400

    Clean up comment function

--- init/km-func.el
+++ init/km-func.el
@@ -68,10 +68,10 @@ user."
   "Comment/uncomment the current line or region"
     (interactive)
     (let (beg end)
-        (if (region-active-p)
-            (setq beg (region-beginning) end (region-end))
-          (setq beg (line-beginning-position) end (line-end-position)))
-        (comment-or-uncomment-region beg end))
+      (if (region-active-p)
+          (setq beg (region-beginning) end (region-end))
+        (setq beg (line-beginning-position) end (line-end-position)))
+      (comment-or-uncomment-region beg end))
     (forward-line))
 
 ;; kill functions
