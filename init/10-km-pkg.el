77e1a33070f0aa1c7358699d0e72b84d17c8e436
Kyle Meyer <kyle@kyleam.com>
2013-11-01 12:49:18 -0400

    Don't load packages post init

--- init/10-km-pkg.el
+++ init/10-km-pkg.el
@@ -3,6 +3,7 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(setq package-enable-at-startup nil)
 (package-initialize)
 
 (require 'cl)
