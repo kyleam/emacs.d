a11581c962f13b01e7be64214ab9271501f22ef6
Kyle Meyer <kyle@kyleam.com>
2013-11-10 23:18:19 -0500

    Add new paredit barf/slurp bindings

--- init/km-elisp.el
+++ init/km-elisp.el
@@ -3,3 +3,10 @@
   t)
 
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+
+(eval-after-load "paredit"
+  '(progn
+    (define-key paredit-mode-map (kbd "C-,") 'paredit-forward-slurp-sexp)
+    (define-key paredit-mode-map (kbd "C-.") 'paredit-forward-barf-sexp)
+    (define-key paredit-mode-map (kbd "C-M-,") 'paredit-backward-slurp-sexp)
+    (define-key paredit-mode-map (kbd "C-M-.") 'paredit-backward-barf-sexp)))
