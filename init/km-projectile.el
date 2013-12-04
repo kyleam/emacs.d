c48e7fda4c7a13af4b29d73f365f0bb7cd2c056e
Kyle Meyer <kyle@kyleam.com>
2013-12-03 21:36:30 -0500

    Function for default projectile switch project
    I've set the project switch action to show the magit status, but this
    function makes the default find file behavior available.

--- init/km-projectile.el
+++ init/km-projectile.el
@@ -4,3 +4,13 @@
 
 (defun km/projectile-magit-status ()
   (magit-status (projectile-project-root)))
+
+(defun km/projectile-switch-project-to-file ()
+  "I have set `projectile-switch-project-action' to
+  `km/projectile-magit-status'. This function gives access to the
+  of default `projectile-find-file'"
+  (interactive)
+  (let ((projectile-switch-project-action 'projectile-find-file))
+    (projectile-switch-project)))
+
+(global-set-key (kbd "C-c p j")  'km/projectile-switch-project-to-file)
