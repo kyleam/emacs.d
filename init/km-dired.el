89bf9ed3c918a1f9e1bf8d8945dcccbc7c8fb941
Kyle Meyer <kyle@kyleam.com>
2013-12-12 23:48:26 -0500

    Add .git and __pycache__ directories to dired omit

--- init/km-dired.el
+++ init/km-dired.el
@@ -1,6 +1,12 @@
 (put 'dired-find-alternate-file 'disabled nil)
 
 (require 'dired-x)
+
+;; .git is present as part of `dired-omit-extensions', but this
+;; seems to only be taken into account if the a non-exension part
+;; exists
+(setq dired-omit-files (concat dired-omit-files "\\|\\.git$\\|__pycache__"))
+
 (setq-default dired-omit-files-p t)
 
 (setq dired-listing-switches "-alh")
