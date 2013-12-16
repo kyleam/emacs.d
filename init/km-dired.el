1882d81c506fbcff31af7bc766ee6d65afb22e3a
Kyle Meyer <kyle@kyleam.com>
2013-12-15 21:42:47 -0500

    Omit intermediate latex files in dired

--- init/km-dired.el
+++ init/km-dired.el
@@ -7,6 +7,18 @@
 ;; exists
 (setq dired-omit-files (concat dired-omit-files "\\|\\.git$\\|__pycache__"))
 
+(defvar km/latex-omit-extensions '(".aux"
+                                   ".fdb_latexmk"
+                                   ".fls"
+                                   ".log"
+                                   ".nav"
+                                   ".out"
+                                   ".snm")
+  "Intermediate LaTeX files")
+
+(setq dired-omit-extensions
+      (append dired-omit-extensions km/latex-omit-extensions))
+
 (setq-default dired-omit-files-p t)
 
 (setq dired-listing-switches "-alh")
