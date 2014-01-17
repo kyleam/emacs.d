0f0b2754c5b5ca1c27bca866de40ae0141b8fbe1
Kyle Meyer <kyle@kyleam.com>
2014-01-16 19:06:34 -0500

    Add settings for bibtex-mode

---
+++ init/km-bib.el
@@ -0,0 +1,12 @@
+;; Make cite key have form <last author last name><year><first word>.
+(setq bibtex-autokey-year-length 4)
+(setq bibtex-autokey-titleword-length nil)
+(setq bibtex-autokey-titlewords-stretch 0)
+(setq bibtex-autokey-titlewords 1)
+(setq bibtex-autokey-year-title-separator "")
+
+(setq bibtex-autokey-titleword-ignore
+      '("A" "An" "On" "The" "Why" "How" "Where" "[0-9].*"))
+
+;; Used by `bibtex-fill-entry'.
+(setq bibtex-align-at-equal-sign t)
