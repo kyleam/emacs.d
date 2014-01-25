377abd682c69fe0f121a85551e061fe3935a15bf
Kyle Meyer <kyle@kyleam.com>
2014-01-25 01:37:19 -0500

    Enforce blank line before org entry

--- init/km-org.el
+++ init/km-org.el
@@ -9,6 +9,7 @@
 (setq org-goto-interface 'outline-path-completion)
 (setq org-src-fontify-natively t)
 (setq org-special-ctrl-k t)
+(setq org-blank-before-new-entry t)
 
 (setq org-capture-templates
       '(("t" "task" entry (file+headline "~/notes/tasks.org" "Inbox")
