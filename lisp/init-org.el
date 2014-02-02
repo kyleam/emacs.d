b19c60bea31bdc6a1d1943d1252123012bdbc4a8
Kyle Meyer <kyle@kyleam.com>
2014-02-01 22:59:41 -0500

    Add note to Org NA state

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,7 +2,7 @@
 
 (setq org-modules '(org-bibtex org-gnus org-info)
       org-log-done t
-      org-todo-keywords '((sequence "TODO" "STARTED" "|" "DONE" "NA"))
+      org-todo-keywords '((sequence "TODO" "STARTED" "|" "DONE" "NA(@)"))
       org-use-speed-commands t
       org-fast-tag-selection-single-key 'expert
       org-catch-invisible-edits  'error
