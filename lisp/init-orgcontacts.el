2f540d092f5c3ab214e143a7ddc0c41f40b67842
Kyle Meyer <kyle@kyleam.com>
2014-02-02 02:20:31 -0500

    Autoload org-contacts-template-name

--- lisp/init-orgcontacts.el
+++ lisp/init-orgcontacts.el
@@ -1,5 +1,9 @@
 (add-to-list 'load-path "~/src/emacs/org-mode/contrib/lisp" t)
 
+(autoload 'org-contacts-template-name "org-contacts"
+  "Try to return the contact name for a template.
+If not found return RETURN-VALUE or something that would ask the user.")
+
 (setq org-contacts-files '("~/notes/contacts.org"))
 
 (add-to-list 'org-capture-templates
