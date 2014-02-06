51da009f1ccfc2df7283e3a85fcb8dcb41c8c8fc
Kyle Meyer <kyle@kyleam.com>
2014-02-05 20:56:36 -0500

    Remove words from bibtex-autokey-titleword-ignore
    These are uncommon first words, so having in them in a citekey should be
    informative enough.

--- lisp/init-bib.el
+++ lisp/init-bib.el
@@ -4,8 +4,7 @@
       bibtex-autokey-titlewords-stretch 0
       bibtex-autokey-titlewords 1
       bibtex-autokey-year-title-separator ""
-      bibtex-autokey-titleword-ignore
-      '("A" "An" "On" "The" "Why" "How" "Where" "[0-9].*"))
+      bibtex-autokey-titleword-ignore '("A" "An" "On" "The"  "[0-9].*"))
 
 (setq bibtex-align-at-equal-sign t)  ; Used by `bibtex-fill-entry'.
 
