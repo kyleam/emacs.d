7a24779b53f6adddf2558717f192415e951d34e9
Kyle Meyer <kyle@kyleam.com>
2014-02-12 23:23:47 -0500

    Remove unnecessary lambdas from yas hooks

--- lisp/init-yas.el
+++ lisp/init-yas.el
@@ -3,12 +3,7 @@
 (require 'yasnippet)
 (yas-reload-all)
 
-(add-hook 'prog-mode-hook
-          '(lambda ()
-             (yas-minor-mode)))
-
-(add-hook 'LaTeX-mode-hook
-          '(lambda ()
-             (yas-minor-mode)))
+(add-hook 'prog-mode-hook 'yas-minor-mode)
+(add-hook 'LaTeX-mode-hook 'yas-minor-mode)
 
 (provide 'init-yas)
