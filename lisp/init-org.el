24d8aa9f74042d42800ff5f61eb3455269f9d43c
Kyle Meyer <kyle@kyleam.com>
2014-02-15 16:18:18 -0500

    Add binding for bog-rename-staged-pdf-to-citekey

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -84,6 +84,7 @@
      (define-prefix-command 'bog-map)
      (define-key org-mode-map (kbd "C-c b") 'bog-map)
      (define-key bog-map "p" 'bog-find-citekey-pdf)
+     (define-key bog-map "r" 'bog-rename-staged-pdf-to-citekey)
      (define-key bog-map "b" 'bog-find-citekey-bib)
      (define-key bog-map "w" 'bog-search-citekey-on-web)
      ;; Avoid conflict when amsmath is loaded.
