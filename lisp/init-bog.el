e22061321f5e788680c74aed9ee6666c797e132b
Kyle Meyer <kyle@kyleam.com>
2014-02-15 16:25:00 -0500

    Set read functions for bog

--- lisp/init-bog.el
+++ lisp/init-bog.el
@@ -1,5 +1,8 @@
 (require 'bog)
 
+(setq bog-completing-read 'ido-completing-read
+      bog-read-file-name 'ido-read-file-name)
+
 (eval-after-load 'org
   '(progn
      (define-prefix-command 'bog-map)
