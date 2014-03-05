58dc2eddb9a4fc02c069d05c90897dbc96fe8ed3
Kyle Meyer <kyle@kyleam.com>
2014-03-04 22:59:08 -0500

    Add bog-goto-citekey-heading-in-* bindings

--- lisp/init-bog.el
+++ lisp/init-bog.el
@@ -10,6 +10,8 @@
      (define-key bog-map "p" 'bog-find-citekey-pdf)
      (define-key bog-map "r" 'bog-rename-staged-pdf-to-citekey)
      (define-key bog-map "b" 'bog-find-citekey-bib)
+     (define-key bog-map "h" 'bog-goto-citekey-heading-in-buffer)
+     (define-key bog-map "H" 'bog-goto-citekey-heading-in-notes)
      (define-key bog-map "w" 'bog-search-citekey-on-web)))
 
 (provide 'init-bog)
