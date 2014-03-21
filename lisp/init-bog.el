99797f4fd7c5d68015349ff6e0c22cd2bfa60cac
Kyle Meyer <kyle@kyleam.com>
2014-03-21 01:42:09 -0400

    Update Bog keymap configuration
    Bog now provides a keymap.

--- lisp/init-bog.el
+++ lisp/init-bog.el
@@ -4,14 +4,6 @@
       bog-read-file-name 'ido-read-file-name)
 
 (eval-after-load 'org
-  '(progn
-     (define-prefix-command 'bog-map)
-     (define-key org-mode-map (kbd "C-c b") 'bog-map)
-     (define-key bog-map "p" 'bog-find-citekey-pdf)
-     (define-key bog-map "r" 'bog-rename-staged-pdf-to-citekey)
-     (define-key bog-map "b" 'bog-find-citekey-bib)
-     (define-key bog-map "h" 'bog-goto-citekey-heading-in-buffer)
-     (define-key bog-map "H" 'bog-goto-citekey-heading-in-notes)
-     (define-key bog-map "w" 'bog-search-citekey-on-web)))
+  '(define-key org-mode-map (kbd "C-c b") bog-mode-map))
 
 (provide 'init-bog)
