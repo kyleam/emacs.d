4fbc5014f61c424fd7be9303a9520ff059370832
Kyle Meyer <kyle@kyleam.com>
2014-03-30 02:47:52 -0400

    Update Bog settings for new minor mode

--- lisp/init-bog.el
+++ lisp/init-bog.el
@@ -1,9 +1,10 @@
-(require 'bog)
+(setq bog-keymap-prefix (kbd "C-c b"))
 
 (setq bog-completing-read 'ido-completing-read
       bog-read-file-name 'ido-read-file-name)
 
-(eval-after-load 'org
-  '(define-key org-mode-map (kbd "C-c b") bog-mode-map))
+(autoload 'bog-mode "bog")
+
+(add-hook 'org-mode-hook 'bog-mode)
 
 (provide 'init-bog)
