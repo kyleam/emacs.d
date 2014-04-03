1e0ef8aeb0b7b4123d267b8ce91d7ace5ad4f0b4
Kyle Meyer <kyle@kyleam.com>
2014-04-02 23:56:06 -0400

    Remove ido customization for Bog

--- lisp/init-bog.el
+++ lisp/init-bog.el
@@ -1,8 +1,5 @@
 (setq bog-keymap-prefix (kbd "C-c b"))
 
-(setq bog-completing-read 'ido-completing-read
-      bog-read-file-name 'ido-read-file-name)
-
 (autoload 'bog-mode "bog")
 
 (add-hook 'org-mode-hook 'bog-mode)
