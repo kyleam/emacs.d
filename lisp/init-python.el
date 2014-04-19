kyleam/emacs.d

commit 6cbfae79602492590f1078d7530f8ac17c480375

Make `create-python-test-file` find file if exists
Instead of raising an error if the file exist, just open file in other
buffer.

lisp/init-python.el
@@ -18,12 +18,12 @@ Unless a prefix argument ARG is given, py.test is also imported."
   (interactive "P")
   (let* ((py-file (file-name-nondirectory buffer-file-name))
          (test-file (concat "test_" py-file)))
-    (when (file-exists-p test-file)
-      (error "Test file %s already exists." test-file))
-    (with-current-buffer (find-file-other-window test-file)
-      (insert (format "import %s\n" (file-name-sans-extension py-file)))
-      (unless arg
-        (insert "import pytest\n")))))
+    (find-file-other-window test-file)
+    (unless (file-exists-p test-file)
+      (with-current-buffer
+          (insert (format "import %s\n" (file-name-sans-extension py-file)))
+        (unless arg
+          (insert "import pytest\n"))))))
 
 (defun km/pytest-compile (arg)
   "Run pytest.

commit b765746c9c8c2c60e1018f48c92b612250776a9e

Fix km/create-python-test-file
6cbfae7 broke this function when the test file doesn't exist. I'm not
sure why I used `with-current-buffer' in the first place, considering
the desired action is to switch to the file in another window.

lisp/init-python.el
@@ -20,10 +20,9 @@ Unless a prefix argument ARG is given, py.test is also imported."
          (test-file (concat "test_" py-file)))
     (find-file-other-window test-file)
     (unless (file-exists-p test-file)
-      (with-current-buffer
-          (insert (format "import %s\n" (file-name-sans-extension py-file)))
-        (unless arg
-          (insert "import pytest\n"))))))
+      (insert (format "import %s\n" (file-name-sans-extension py-file)))
+      (unless arg
+        (insert "import pytest\n")))))
 
 (defun km/pytest-compile (arg)
   "Run pytest.