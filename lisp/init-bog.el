502830119fddb13d963ed2bdcaf852eb809ef34c
Kyle Meyer <kyle@kyleam.com>
2014-05-02 23:43:38 -0400

    Add Bog commander binding

--- lisp/init-bog.el
+++ lisp/init-bog.el
@@ -1,5 +1,7 @@
 (setq bog-keymap-prefix (kbd "C-c b"))
+(global-set-key (kbd "C-c B") 'bog-commander)
 
+(autoload 'bog-commander "bog")
 (autoload 'bog-mode "bog")
 
 (add-hook 'org-mode-hook 'bog-mode)
