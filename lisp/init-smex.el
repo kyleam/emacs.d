c199ee7a11ccc4e7935c1407987fcb2bfaa431f5
Kyle Meyer <kyle@kyleam.com>
2014-05-07 23:01:07 -0400

    Clean up init-smex.el

--- lisp/init-smex.el
+++ lisp/init-smex.el
@@ -1,12 +1,8 @@
 (require-package 'smex)
 
-;; http://www.juanrubio.me/2011/11/emacs-smex-m-x-do-not-like-typing/
 (smex-initialize)
-;; smex bound in km-evil.ex (,x).
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;; Old M-x
-(global-set-key (kbd "M-x") 'execute-extended-command)
 
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (key-chord-define-global ",x" 'smex)
 
 (provide 'init-smex)
