debbf36e46914ee9e2b75e3c648e043f4d5b1a27
Kyle Meyer <kyle@kyleam.com>
2014-05-12 21:21:25 -0400

    Install and setup ace-window

--- lisp/init-ace.el
+++ lisp/init-ace.el
@@ -1,10 +1,14 @@
 (require-package 'ace-jump-mode)
 (require-package 'ace-link)
+(require-package 'ace-window)
 
 (key-chord-define-global ";a" 'ace-jump-mode)
 
 (setq ace-jump-mode-scope 'frame)
 
 (ace-link-setup-default)
+
+(define-key window-map "a" 'ace-window)
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 
 (provide 'init-ace)
