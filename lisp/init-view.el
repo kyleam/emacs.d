0dc8f60429f81febc53d3f9c8ba42dd51034028b
Kyle Meyer <kyle@kyleam.com>
2014-06-18 00:26:40 -0400

    Define {forward,backward}-word in view-mode-map

--- lisp/init-view.el
+++ lisp/init-view.el
@@ -5,6 +5,8 @@
 (after 'view
   (define-key view-mode-map "l" 'recenter-top-bottom)
   (define-key view-mode-map "a" 'ace-jump-mode)
+  (define-key view-mode-map "f" 'forward-word)
+  (define-key view-mode-map "b" 'backward-word)
   (define-key view-mode-map "j" 'imenu))
 
 (provide 'init-view)
