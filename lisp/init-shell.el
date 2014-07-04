55b4bba4b4a1847db3ea2b0cce5de98f3bd8e479
Kyle Meyer <kyle@kyleam.com>
2014-07-04 01:35:14 -0400

    Don't add PKGBUILD to auto-mode-alist
    This is (now?) added by pkgbuild-mode.

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -4,6 +4,5 @@
 
 (add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
 (add-to-list 'auto-mode-alist '("\\.*rc$" . conf-unix-mode))
-(add-to-list 'auto-mode-alist '("PKGBUILD" . pkgbuild-mode))
 
 (provide 'init-shell)
