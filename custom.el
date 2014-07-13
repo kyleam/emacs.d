354b1c59e8139dba770e34bbd757448dbdcedb42
Kyle Meyer <kyle@kyleam.com>
2014-07-12 22:02:56 -0400

    Change dired-subtree faces

--- custom.el
+++ custom.el
@@ -9,4 +9,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(dired-subtree-depth-1-face ((t (:background "gray80"))))
+ '(dired-subtree-depth-2-face ((t (:background "gray90"))))
+ '(dired-subtree-depth-3-face ((t (:background "gray95")))))
