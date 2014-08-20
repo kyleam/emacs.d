ba9c409f0e9658e286d0c0763e95a9ae23a694d9
Kyle Meyer <kyle@kyleam.com>
2014-08-19 21:44:39 -0400

    Add {forward,backward}-paragraph to view-mode-map

--- lisp/init-view.el
+++ lisp/init-view.el
@@ -7,6 +7,8 @@
   (define-key view-mode-map "a" 'ace-jump-mode)
   (define-key view-mode-map "f" 'forward-word)
   (define-key view-mode-map "b" 'backward-word)
+  (define-key view-mode-map "]" 'forward-paragraph)
+  (define-key view-mode-map "[" 'backward-paragraph)
   (define-key view-mode-map "j" 'imenu))
 
 (provide 'init-view)
