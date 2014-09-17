996004ee913274832cb24d36dec626f7962fb12e
Kyle Meyer <kyle@kyleam.com>
2014-09-17 01:16:14 -0400

    Stop interactive-haskell from binding 'C-c c'

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -3,4 +3,8 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 
+(after 'haskell-process
+  ;; Unbind `haskell-process-cabal' from user's key.
+  (define-key interactive-haskell-mode-map (kbd "C-c c") nil))
+
 (provide 'init-haskell)
