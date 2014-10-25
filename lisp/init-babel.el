1d82ca6d7d6a6150a276984c62d10581b9deb10e
Kyle Meyer <kyle@kyleam.com>
2014-10-24 23:55:36 -0400

    Remove old Org export settings
    This was for Org < 8.0.

--- lisp/init-babel.el
+++ lisp/init-babel.el
@@ -10,16 +10,4 @@
 ;; Don't ask for confirmation before running code.
 (setq org-confirm-babel-evaluate nil)
 
-;; Babel minted latex export
-;; Modified from
-;; http://orgmode.org/worg/org-tutorials/org-latex-export.html.
-(setq org-export-latex-listings 'minted
-      org-export-latex-custom-lang-environments '((R "rcode")
-                                                  (sh "shcode")
-                                                  (python "pythoncode")))
-(setq org-latex-to-pdf-process
-      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
-
 (provide 'init-babel)
