c7cca9ecb8a6f2b45f52709ec64d84990802167b
Kyle Meyer <kyle@kyleam.com>
2014-11-02 00:50:26 -0400

    Don't let paredit clobber M-s map

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -10,6 +10,10 @@
   (define-key paredit-mode-map (kbd "C-,") 'paredit-forward-slurp-sexp)
   (define-key paredit-mode-map (kbd "C-.") 'paredit-forward-barf-sexp)
   (define-key paredit-mode-map (kbd "C-M-,") 'paredit-backward-slurp-sexp)
-  (define-key paredit-mode-map (kbd "C-M-.") 'paredit-backward-barf-sexp))
+  (define-key paredit-mode-map (kbd "C-M-.") 'paredit-backward-barf-sexp)
+
+  ;; Don't let `paredit-splice-sexp' override default 'M-s' map.
+  (define-key paredit-mode-map (kbd "M-s") nil)
+  (define-key paredit-mode-map (kbd "M-p") 'paredit-splice-sexp))
 
 (provide 'init-elisp)
