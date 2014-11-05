da7dd5027472f231533dcc40e2e19d3c83874f12
Kyle Meyer <kyle@kyleam.com>
2014-11-05 00:23:48 -0500

    Handle compiling non-project files with Snakemake
    Allow option to prevent km/snakemake-compile-project-file-at-point from
    append project path, but keep default-directory set to project root.
    This is useful if compiling from rule files that already list the file
    with its full path.

--- lisp/init-snakemake.el
+++ lisp/init-snakemake.el
@@ -11,12 +11,17 @@
              (set (make-local-variable 'compile-command)
                   (snakemake-compile-command))))
 
-(defun km/snakemake-compile-project-file-at-point ()
-  "Run Snakemake to produce project file at point."
-  (interactive)
-  (let ((compile-command (concat (snakemake-compile-command) " "
-                                 (km/project-filename-at-point)))
-        (default-directory (projectile-project-root)))
+(defun km/snakemake-compile-project-file-at-point (arg)
+  "Run Snakemake to produce project file at point.
+With prefix ARG, use file name as is, without trying to append
+the project's path."
+  (interactive "P")
+  (let* ((fname (if arg
+                    (thing-at-point 'filename)
+                  (km/project-filename-at-point)))
+         (compile-command (concat (snakemake-compile-command) " "
+                                  fname))
+         (default-directory (projectile-project-root)))
     (call-interactively 'compile)))
 
 (autoload 'snakemake-compile-command "snakemake-mode")
