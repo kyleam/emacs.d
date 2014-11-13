693aa551f5741b8190350629f818770c60fdc5cb
Kyle Meyer <kyle@kyleam.com>
2014-11-12 23:49:57 -0500

    Use bog autoloads file

--- lisp/init-bog.el
+++ lisp/init-bog.el
@@ -1,10 +1,9 @@
 (add-to-list 'load-path "~/src/emacs/bog/")
 
+(require 'bog-autoloads)
+
 (setq bog-keymap-prefix (kbd "C-c b"))
 (global-set-key (kbd "C-c B") 'bog-commander)
-
-(autoload 'bog-commander "bog")
-(autoload 'bog-mode "bog")
 
 (add-hook 'org-mode-hook 'bog-mode)
 
