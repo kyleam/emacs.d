82cb3ff79b3391f958464d33b903f8947aa95121
Kyle Meyer <kyle@kyleam.com>
2014-12-08 01:40:10 -0500

    Enable haskell-doc-mode

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,6 +1,7 @@
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc)
 
 (after 'haskell-process
   ;; Unbind `haskell-process-cabal' from user's key.
