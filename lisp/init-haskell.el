08b0735bf8b066e509a2d6e1af369d9dcab37621
Kyle Meyer <kyle@kyleam.com>
2015-01-13 22:51:00 -0500

    Update interactive-haskell-mode setup

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -3,8 +3,14 @@
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc)
 
-(after 'haskell-process
-  ;; Unbind `haskell-process-cabal' from user's key.
-  (define-key interactive-haskell-mode-map (kbd "C-c c") nil))
+(after 'haskell-mode
+  (define-key haskell-mode-map (kbd "C-x C-d") nil)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+  (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c M-.") nil)
+  (define-key haskell-mode-map (kbd "C-c C-d") nil))
 
 (provide 'init-haskell)
