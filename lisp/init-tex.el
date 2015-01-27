dde18243db3828da9b257e1efbc82b23854705ef
Kyle Meyer <kyle@kyleam.com>
2015-01-27 01:23:54 -0500

    Load latex-mode, not tex-mode, for *.tex files

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -3,6 +3,8 @@
 
 (put 'LaTeX-narrow-to-environment 'disabled nil)
 
+(add-to-list 'auto-mode-alist '("\\.[tT]e[xX]\\'" . latex-mode))
+
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 
 (provide 'init-tex)
