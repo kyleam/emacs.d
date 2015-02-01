eede581862d46ccde4313ddb13b97e63082909ee
Kyle Meyer <kyle@kyleam.com>
2015-02-01 18:11:47 -0500

    Add kill buffer variant

--- lisp/init-buffers.el
+++ lisp/init-buffers.el
@@ -13,7 +13,20 @@
   (save-buffer)
   (kill-this-buffer))
 
-(global-set-key (kbd "C-x K") 'kill-buffer-and-window)
+(defun km/kill-buffer (&optional arg)
+  "Kill this buffer.
+With single C-u, prompt for buffer to kill.  With double C-u,
+kill this buffer and the window."
+  (interactive "P")
+  (cond
+   ((not arg)
+    (kill-buffer))
+   ((equal arg '(16))
+    (kill-buffer-and-window))
+   (t
+    (call-interactively #'kill-buffer))))
+
+(global-set-key (kbd "C-x K") 'km/kill-buffer)
 
 (key-chord-define-global ",d" 'km/save-and-kill-buffer)
 (key-chord-define-global ",s" 'save-buffer)
