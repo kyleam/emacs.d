831c69beda4b6803f8b0ddd8f19c187782c1d7bc
Kyle Meyer <kyle@kyleam.com>
2015-02-01 19:15:42 -0500

    Turn off yas fallback behavior
    It's not bound to a key with any other binding.

--- lisp/init-yas.el
+++ lisp/init-yas.el
@@ -1,6 +1,8 @@
 
 (require 'yasnippet)
 
+(setq yas-fallback-behavior nil)
+
 (define-key yas-minor-mode-map (kbd "C-c i") 'yas-expand)
 (define-key yas-minor-mode-map (kbd "<tab>") nil)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
