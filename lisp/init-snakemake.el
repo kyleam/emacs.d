633b12c952300d55dabaf27e3e305d4e2d481cfb
Kyle Meyer <kyle@kyleam.com>
2015-02-03 02:16:47 -0500

    Correct typo in comment

--- lisp/init-snakemake.el
+++ lisp/init-snakemake.el
@@ -5,10 +5,10 @@
 
 (setq snakemake-compile-command-options '("-p"))
 
-;; Although `compile-command' is set when snakemake-mode is derived from
-;; Python mode, I need to define it again here because I have a Python
-;; mode hook that sets `compile-command', which overides the snakemake
-;; version.
+;; Although `compile-command' is set when snakemake-mode is derived
+;; from Python mode, I need to define it again here because I have a
+;; Python mode hook that sets `compile-command', which overrides the
+;; snakemake version.
 (add-hook 'snakemake-mode-hook
           (lambda ()
             (set (make-local-variable 'compile-command)
