616f49656e1bebd76c8261eacc3d9259673fbef4
Kyle Meyer <kyle@kyleam.com>
2015-02-02 22:48:49 -0500

    Change default ESS style
    Keep all continued statements and expression at the original level.

--- lisp/init-ess.el
+++ lisp/init-ess.el
@@ -3,6 +3,20 @@
 
 (setq ess-smart-S-assign-key ";")
 
+(after 'ess-mode
+  (ess-add-style
+   'km
+   '((ess-first-continued-statement-offset . 4)
+     (ess-continued-statement-offset . 0)
+     (ess-arg-function-offset-new-line . nil)
+     (ess-arg-function-offset . nil)
+     (ess-expression-offset . nil)
+     (ess-indent-level . 4)
+     (ess-brace-offset . 0)
+     (ess-else-offset . 0)
+     (ess-close-brace-offset . 0))))
+(setq ess-default-style 'km)
+
 (add-to-list 'auto-mode-alist '("\\.[rR]\\'" . R-mode))
 
 (provide 'init-ess)
