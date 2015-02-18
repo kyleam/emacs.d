406d541f4381173ce83a4086964bbffbf7a6a3e2
Kyle Meyer <kyle@kyleam.com>
2015-02-18 02:02:05 -0500

    Remove 'C-c &' prefix for yasnippet

--- lisp/init-yas.el
+++ lisp/init-yas.el
@@ -4,6 +4,9 @@
 (setq yas-fallback-behavior nil)
 
 (define-key yas-minor-mode-map (kbd "C-c i") 'yas-expand)
+;; Remove commands with 'C-c &' prefix, which conflicts with
+;; `org-mark-ring-goto' binding'
+(define-key yas-minor-mode-map (kbd "C-c &") nil)
 (define-key yas-minor-mode-map (kbd "<tab>") nil)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 
