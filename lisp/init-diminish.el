5ad081fb1bcddbc360b355506a6162cb24ce77a4
Kyle Meyer <kyle@kyleam.com>
2015-05-13 22:21:01 -0400

    init-diminish.el: Update for Magit changes
    Commit a3c7c70cdf4

--- lisp/init-diminish.el
+++ lisp/init-diminish.el
@@ -4,7 +4,6 @@
 (after 'helm-mode (diminish 'helm-mode))
 (after 'flyspell (diminish 'flyspell-mode "Fy"))
 (after 'paredit (diminish 'paredit-mode " Pe"))
-(after 'magit (diminish 'magit-auto-revert-mode))
 (after 'mml (diminish 'mml-mode "Ml"))
 (after 'org (diminish 'orgstruct-mode "Os"))
 (after 'org-table (diminish 'orgtbl-mode "Ot"))
