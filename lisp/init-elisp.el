bc0363f06dbbfa507db09c368de0963c53ee5947
Kyle Meyer <kyle@kyleam.com>
2015-08-01 00:23:45 -0400

    Stop semantic from taking over elisp imenu

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -6,6 +6,13 @@
 ;; This likely breaks lispy's outline functions, but I don't use them.
 (add-hook 'lispy-mode-hook 'km/elisp-set-outline-vars)
 
+(after 'lispy
+  ;; This is ugly, but I haven't found another way to stop
+  ;; `imenu-create-index-function' from being set to
+  ;; `semantic-create-imenu-index'.  Trying to set it in
+  ;; `emacs-lisp-mode-hook' or `lispy-mode-hook' doesn't work.
+  (defalias 'semantic-create-imenu-index 'imenu-default-create-index-function))
+
 (defun km/elisp-outline-level ()
   (and (looking-at (concat "^" outline-regexp))
        (- (match-end 0) (match-beginning 0) 3)))
