b074720c3f26211e5fcec82468436f9ee9b55740
Kyle Meyer <kyle@kyleam.com>
2015-09-09 23:47:02 -0400

    Set font-latex-fontify-sectioning to 'color'

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -1,6 +1,8 @@
 
 (setq reftex-default-bibliography '("refs.bib"))
 
+(setq font-latex-fontify-sectioning 'color)
+
 (put 'LaTeX-narrow-to-environment 'disabled nil)
 
 (add-to-list 'auto-mode-alist '("\\.[tT]e[xX]\\'" . latex-mode))
