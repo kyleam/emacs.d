cf08f39de1ab2da2374f88ef969217aefec5187b
Kyle Meyer <kyle@kyleam.com>
2015-11-01 19:49:26 -0500

    Turn off haskell process tips

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,4 +1,6 @@
 
+(setq haskell-process-show-debug-tips nil)
+
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc)
